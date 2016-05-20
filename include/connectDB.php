0a7dd2f97e24f78b2cb06361bd73414da1e3ee7f
Blake Stiller <stiller.blake@gmail.com>
2016-05-20 16:27:33 -0400

    JS-Intro PHP
    Added $dbConn to mysqlierror()

--- include/connectDB.php
+++ include/connectDB.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL ^ E_NOTICE);
 $dbConn = mysqli_connect('localhost', 'lizard2', 'lizard', 'gregs_gambits');
 if(!$dbConn){
-die('Could not connect: ' . mysqli_error());
+die('Could not connect: ' . mysqli_error($dbConn));
 }
 //$dbObj = mysql_select_db('gregs_gambits', $dbConn);
 
